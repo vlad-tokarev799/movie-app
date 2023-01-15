218f38d27ebe705c5247ae638fc293c055053d7b
vlad-tokarev799 <vlad.tokarev.200247@gmail.com>
2023-01-15 18:53:24 +0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-const str = 'str';
-console.log(str);
+// const str = 'str';
 
 root.render(<React.StrictMode></React.StrictMode>);
