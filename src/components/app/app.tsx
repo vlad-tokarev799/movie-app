0f3def2cde463268b0155c7681aeedcbc5244f94
vlad-tokarev799 <vlad.tokarev.200247@gmail.com>
2023-01-15 18:59:37 +0800

    test

---
+++ src/components/app/app.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import './app.scss';
+
+export default class App extends Component {
+  render() {
+    return <div className="App">this is TypeScript app</div>;
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-// const str = 'str';
-
 root.render(<React.StrictMode></React.StrictMode>);
