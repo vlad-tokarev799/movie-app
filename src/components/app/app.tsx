dd038f520f1de11d0fd654490ee98e254ee5a7bc
vlad-tokarev799 <vlad.tokarev.200247@gmail.com>
2023-01-15 20:52:22 +0800

    sync

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
+// import MovieDbService from '../../services/movie-db-service';
 
 import './app.scss';
 
 export default class App extends Component {
-  state = {
-    text: 'this is TypeScript app',
-  };
-
   render() {
-    const { text } = this.state;
-
-    return <div className="app">{text}</div>;
+    return <div className="app"></div>;
   }
 }
---
+++ src/components/movies-list/movies-list.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import './movies-list.scss';
+
+export default class MoviesList extends Component {
+  render() {
+    return <div></div>;
+  }
+}
